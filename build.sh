711acdcf011528f70adbdabea2e2c8d4c02a5474
Kevin Solkowski <ksolkowski@gmail.com>
2014-04-23 16:32:47 -0500

    this is kinda working.
    the requests don't have any error checking/anything cool. and it also
    returns gross XML but it's still a start

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ if [ -e mcts_bustime-0.0.1.gem ]; then
 fi 
 
 gem build mcts_bustime.gemspec
-sudo gem uninstall mcts_bustime
-sudo gem install mcts_bustime-0.0.1.gem
+gem uninstall mcts_bustime
+gem install mcts_bustime-0.0.1.gem
--- lib/mcts_bustime.rb
+++ lib/mcts_bustime.rb
@@ -10,6 +10,23 @@ class MctsBustime
     @url = 'http://realtime.ridemcts.com/bustime/api/v1/'
   end
 
-  
+
+  def call(method, params = {})
+    api_url = @url + method
+    uri_params = URI.encode_www_form params
+    response = RestClient::Request.execute(:method => :get, :url => api_url+"?key=#{@api_key}&#{uri_params}", :timeout => -1)
+
+    response
+  end
+
+  def method_missing(method, *args)
+    method = method.to_s
+    call("#{method}", *args)
+  end
+
+  def request_whois
+    response = RestClient::Request.execute(:method => :get, :url => 'http://realtime.ridemcts.com/bustime/api/v1/getvehicles?key=GxV35GtQW6Zw3HEE6x5QeJdZR&rt=21&format=json', :timeout => -1)
+    puts "#{@url}/gettime?key=#{@api_key}"
+  end
   # Your code goes here...
 end
